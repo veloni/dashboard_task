bb9b12a37bcd5cacfd583c747bb2643c25a244fc
egorko <dewvify@gmail.com>
2020-11-12 22:09:42 +0500

    fix bag maxDayMonth, changeperiodmenu

--- loadTask.js
+++ loadTask.js
@@ -89,11 +89,6 @@ const renderTasks = () => {
 window.testFunction = renderTasks;
 renderTasks();
 
-
-
-
-
-
 //узнаём проценты тасков
 // isolated
 // check and rework variables
@@ -201,21 +196,17 @@ maxDateMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0).ge
 day = startDate.getDate();
 month = startDate.getMonth();
 
-console.log(maxDateMonth);
-console.log(day);
-console.log(month);
-
 let daysInYearMassive = [];
 let daysDateMassive = [];
-
-
 
 function daysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
   }
 
+
+
   function daysInYear(year) {
-    var days = 0;
+    let days = 0;
 
     for(var month = 1; month <= 12; month++) {
       days += daysInMonth(month, year);
@@ -227,22 +218,30 @@ function daysInMonth(month, year) {
   let daysYears = daysInYear(2020);
   console.log(daysYears);
 
+
   // move to config
   monthMassive = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
 // rewrite to declarative style
+
 function loadAllDate(){
-    for(i=0;i<daysYears*2;i++){
+    for(i=0;i<1+daysYears*2;i++){
+
+        if(day <= daysInMonth(month, 2020)) 
+        {
         daysInYearMassive.push(day);
         daysInYearMassive.push(month);
-
         day += 1;
-        if(day > daysInMonth(month, 2020)) {
-            month +=1;
+    }
+ else
+        {
             day = 1;
+            month +=1;
         }
     }
 }
+
+console.log(daysInYearMassive);
 
 loadAllDate();
 
@@ -311,7 +310,6 @@ console.log(); */
 
 let canvasGraph = document.getElementById('js-graph');
 let drawGraph = canvasGraph.getContext('2d');
-
 
 
 drawGraph.lineWidth = 1;
--- script.js
+++ script.js
@@ -71,6 +71,8 @@ listener();
 // isolate
 const switchPeriod = document.querySelector('.js-switch-period');
 const menuSwitchPeriod = document.querySelectorAll('.js-menu-switch-period');
+const boxSwitchMenu = document.querySelector('.box-switch-menu');
+const headerTask = document.querySelector('.box-text-header-bar-task');
 let saveMonthTop;
 let saveYearTop;
 
@@ -82,7 +84,7 @@ function AddDn() {
     }
   });
 }
-AddDn();
+AddDn();//Если функция вызывается внутри клика не срабатывает
 
 // to listener
 switchPeriod.onclick = function () {
@@ -90,26 +92,46 @@ switchPeriod.onclick = function () {
     item.classList.remove('dn');
 
   });
+  headerTask.style.height = '35px'; // magic
+  boxSwitchMenu.style.paddingTop = '35px'; 
 }
 
 // rework
 menuSwitchPeriod[0].onclick = function () {
   menuSwitchPeriod[0].innerHTML = menuSwitchPeriod[0].textContent;
-  AddDn();
+  menuSwitchPeriod.forEach((item, index) => {
+    if (index > 0) {
+      item.classList.add('dn');
+    }
+  });
+  boxSwitchMenu.style.paddingTop = '0';
+  headerTask.style.height = 'none';
 }
 
 menuSwitchPeriod[1].onclick = function () {
   const savePeriod = menuSwitchPeriod[0].innerHTML ;
   menuSwitchPeriod[0].innerHTML = menuSwitchPeriod[1].textContent;
   menuSwitchPeriod[1].innerHTML = savePeriod;
-  AddDn();
+  menuSwitchPeriod.forEach((item, index) => {
+    if (index > 0) {
+      item.classList.add('dn');
+    }
+  });
+  boxSwitchMenu.style.paddingTop = '0';
+  headerTask.style.height = 'none';
 }
 
 menuSwitchPeriod[2].onclick = function () {
   const savePeriod = menuSwitchPeriod[0].innerHTML ;
   menuSwitchPeriod[0].innerHTML = menuSwitchPeriod[2].textContent;
   menuSwitchPeriod[2].innerHTML = savePeriod;
-  AddDn();
+  menuSwitchPeriod.forEach((item, index) => {
+    if (index > 0) {
+      item.classList.add('dn');
+    }
+  });
+  boxSwitchMenu.style.paddingTop = '0';
+  headerTask.style.height = 'none';
 }
 
 // Calendar
